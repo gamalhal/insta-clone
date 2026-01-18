8477979a060a4c2c80a3a8c16d651960f837efcd
Gamal Hal <gamalhal.chi@gmail.com>
2026-01-18 18:22:04 +0200

    refactor: restructure HomePage component and update routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage.jsx'
+import HomePage from './pages/HomePage/HomePage.jsx'
 function App() {
  
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
 import { Provider } from "./components/ui/provider"
-import React from "react"
-
+import { BrowserRouter } from "react-router-dom"
 import App from "./App"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <browserRouter> 
-     <Provider theme={"theme-dark"}>
-      <App />
-    </Provider>
-    </browserRouter>
+    <BrowserRouter> 
+      <Provider theme={"theme-dark"}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
